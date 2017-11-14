ce4039ac36967771a877741b0de33719cdee1186
Kallio95 <kallio95@gmail.com>
2017-11-14 11:39:21 +0200

    Add files via upload

--- php/auth.php
+++ php/auth.php
@@ -28,10 +28,37 @@
 		if (mysqli_num_rows($result) != 1) {
 			$resp["code"] = 1;
 		} else {
-			$_SESSION["user"] = $user;
-			$_SESSION["poll"] = $poll;
 			
-			$resp["code"] = 0;
+			$query = $conn->prepare("SELECT status, startdate, enddate
+									FROM poll
+									WHERE id=?");
+			$query->bind_param("i", $poll);
+			$query->execute();
+			
+			$result = $query->get_result();
+			
+			if (mysqli_num_rows($result) == 1) {
+				
+				$row = $result->fetch_assoc();
+				
+				$status = $row["status"];
+				$start = $row["startdate"];
+				$end = $row["enddate"];
+				
+				if ($status == 2) {
+					
+					$_SESSION["user"] = $user;
+					$_SESSION["poll"] = $poll;
+					$resp["code"] = 0;
+					
+				} else {
+					// Not open
+					$resp["code"] = 2;
+				}
+				
+			} else {
+				$resp["code"] = 1;
+			}
 		}
 		
 		
